38d716649a599fe970e0ad1a202bd0194ee506c4
S Stella <sstella2001@gmail.com>
2022-05-16 21:34:31 -0400

    Creating a Selenium scrapper

---
+++ Gorvernance/governance.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+
+DRIVER_PATH = '/path/to/chromedriver'
+driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+driver.get('https://gov.uniswap.org')
+
+
+def scroll_down():
+    """A method for scrolling the page."""
+
+    # Get scroll height.
+    last_height = self.driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+
+        # Scroll down to the bottom.
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        # Wait to load the page.
+        time.sleep(2)
+
+        # Calculate new scroll height and compare with last scroll height.
+        new_height = self.driver.execute_script("return document.body.scrollHeight")
+
+        if new_height == last_height:
+
+            break
+
+        last_height = new_heightimport urllib.request
